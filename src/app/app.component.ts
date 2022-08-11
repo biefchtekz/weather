fc0d38150ecca35f3009735c8f1c606601833260
biefchtekz <sydoruk2oo3@gmail.com>
2022-08-11 18:16:03 +0200

    upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CountriesService} from "./services/countries.service";
+import {Country, Weather} from "./interfaces/interfaces";
+import {WeatherDbService} from "./services/weather-db.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'weather';
+export class AppComponent implements OnInit {
+
+  countries: any
+  searchCountry = ''
+  searchCity = ''
+  // @ts-ignore
+  Country: Country
+  City = ''
+  // @ts-ignore
+  Weather: Weather
+
+  constructor(
+    private getCountries: CountriesService,
+    private weather: WeatherDbService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.getCountries.getCountries().subscribe(
+      log => {
+        // @ts-ignore
+        this.countries = log.data
+
+      }
+    )
+
+  }
+
+  changeCountry(country: string) {
+    this.loadCountry(country)
+  }
+
+  reloadInput(event: Event) {
+    // @ts-ignore
+    this.loadCountry(event.target.value)
+  }
+
+  loadCountry(country: string) {
+    for (let obj of this.countries) {
+      if (obj.country === country) {
+        this.Country = obj
+        break;
+      } else {
+        this.Country = {
+          country:  '',
+          cities: []
+        }
+      }
+    }
+  }
+
+  getWeather() {
+    this.weather.getWeather(this.City).subscribe(
+      log => {
+        this.Weather = log
+        console.log(log)
+      }
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import { SearchCountryPipe } from './pipes/search-country.pipe';
+import {FormsModule} from "@angular/forms";
+import { SearchCityPipe } from './pipes/search-city.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchCountryPipe,
+    SearchCityPipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
